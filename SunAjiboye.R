11e89366deb42c92b874d40e0a6413f9f5b6610c
Sun <your-email@example.com>
2025-03-07 10:52:37 +0000

    Creating conflict

---
+++ SunAjiboye.R
@@ -0,0 +1,13 @@
+# Load all .R files in the R/ folder
+r_files <- list.files(path = "R/", pattern = "\\.R$", full.names = TRUE)
+lapply(r_files, source)
+
+# Get your hand dirty
+get_your_hands_dirty <- function(student_name = 'Sun_Ajiboye'){
+  text <- paste0('Hello my dear ', student_name, '! Time for you to get your hands dirty!')
+  return(text)
+
+}
+
+result0 <- get_your_hands_dirty(8/2)
+print(result0)
